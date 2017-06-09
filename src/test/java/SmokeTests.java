4509124bce8f2e3d78d3af63123d0fc7efd41960
s.garmaev <garmsayan@gmail.com>
2017-06-09 15:11:14 +0300

    Pattern Page Object modified

--- src/test/java/SmokeTests.java
+++ src/test/java/SmokeTests.java
@@ -21,56 +21,57 @@ public class SmokeTests extends BaseTests {
         createAccountPage.clickOnSaveButton();
         Assert.assertEquals(createAccountPage.getFailureMessage(),"Your request failed to complete.");
         Assert.assertEquals(createAccountPage.getErrorMessage(),"Text verification failed.");
+
     }
 
-//    @Test
-//    public void checkTitleLinkOfSpecialOfferCarousel(){
-//        String titleName="HEN WITH GREEN APPLES";
-//        welcomePage=openWelcomePage();
-//        Assert.assertEquals(welcomePage.clickOnTitleOfSpecialOfferCarousel(titleName).getTitle(),titleName);
-//    }
-//
-//    @Test
-//    public void checkNextFunctionOfSpecialOfferCarusel() throws InterruptedException {
-//        SpecialOffersCarouselContainer[] VisibleSpecialOffersCarouselContainersBeforeClick;
-//        welcomePage=openWelcomePage();
-//        VisibleSpecialOffersCarouselContainersBeforeClick=welcomePage.getVisibleSpecialOfferCarouselContainers();
-//        welcomePage.clickOnNextSpecialOfferCarusel();
-//        Assert.assertNotEquals(welcomePage.getVisibleSpecialOfferCarouselContainers(),VisibleSpecialOffersCarouselContainersBeforeClick, "The arrays shouldn't be equal");
-//    }
-//
-//    @Test
-//    public void maxDepthMenuItemOpensAppropriatePage() throws InterruptedException {
-//        welcomePage = openWelcomePage();
-//        welcomePage.selectItemHeaderMenu("PRODUCTS");
-//        productsPage = getProductsPage();
-//        Assert.assertEquals(productsPage.clickOnFirstMaxDepthItemsMenu(), productsPage.getTitle());
-//    }
-//
-//    @Test
-//    public void checkHeaderMenuNames() {
-//        String[] expectedMenuItemNames = {"WELCOME", "PRODUCTS", "SPECIAL OFFERS", "BLOGS", "CONTACT US"};
-//        welcomePage = openWelcomePage();
-//        Assert.assertEquals(welcomePage.getMenuItemNames().toArray(), expectedMenuItemNames);
-//    }
-//
-//    @DataProvider(name = "headerMenuLinkRelatedData")
-//    public Object[][] createHeaderMenuLinkRelatedData() {
-//        return new Object[][]{
-//                {"WELCOME", "http://demo.ict4apps.com/welcome", ""},
-//                {"PRODUCTS", "http://demo.ict4apps.com/products", "Products"},
-//                {"SPECIAL OFFERS", "http://demo.ict4apps.com/special-offers", "Special Offers"},
-//                {"BLOGS", "http://demo.ict4apps.com/blogs", "Blogs"},
-//                {"CONTACT US", "http://demo.ict4apps.com/contact-us", "Contact Us"}};
-//    }
-//
-//    @Test(dataProvider = "headerMenuLinkRelatedData")
-//    public void checkHeaderMenuLinks(String[] data) throws InterruptedException {
-//        welcomePage = openWelcomePage();
-//        welcomePage.selectItemHeaderMenu(data[0]);
-//        Assert.assertEquals(data[1], getCurrentUrl());
-//        Assert.assertEquals(data[2], welcomePage.getLastBreadCrumb());
-//    }
+    @Test
+    public void checkTitleLinkOfSpecialOfferCarousel(){
+        String titleName="HEN WITH GREEN APPLES";
+        welcomePage=openWelcomePage();
+        Assert.assertEquals(welcomePage.clickOnTitleOfSpecialOfferCarousel(titleName).getTitle(),titleName);
+    }
+
+    @Test
+    public void checkNextFunctionOfSpecialOfferCarusel() throws InterruptedException {
+        SpecialOffersCarouselContainer[] VisibleSpecialOffersCarouselContainersBeforeClick;
+        welcomePage=openWelcomePage();
+        VisibleSpecialOffersCarouselContainersBeforeClick=welcomePage.getVisibleSpecialOfferCarouselContainers();
+        welcomePage.clickOnNextSpecialOfferCarusel();
+        Assert.assertNotEquals(welcomePage.getVisibleSpecialOfferCarouselContainers(),VisibleSpecialOffersCarouselContainersBeforeClick, "The arrays shouldn't be equal");
+    }
+
+    @Test
+    public void maxDepthMenuItemOpensAppropriatePage() throws InterruptedException {
+        welcomePage = openWelcomePage();
+        welcomePage.selectItemHeaderMenu("PRODUCTS");
+        productsPage = getProductsPage();
+        Assert.assertEquals(productsPage.clickOnFirstMaxDepthItemsMenu(), productsPage.getTitle());
+    }
+
+    @Test
+    public void checkHeaderMenuNames() {
+        String[] expectedMenuItemNames = {"WELCOME", "PRODUCTS", "SPECIAL OFFERS", "BLOGS", "CONTACT US"};
+        welcomePage = openWelcomePage();
+        Assert.assertEquals(welcomePage.getMenuItemNames().toArray(), expectedMenuItemNames);
+    }
+
+    @DataProvider(name = "headerMenuLinkRelatedData")
+    public Object[][] createHeaderMenuLinkRelatedData() {
+        return new Object[][]{
+                {"WELCOME", "http://demo.ict4apps.com/welcome", ""},
+                {"PRODUCTS", "http://demo.ict4apps.com/products", "Products"},
+                {"SPECIAL OFFERS", "http://demo.ict4apps.com/special-offers", "Special Offers"},
+                {"BLOGS", "http://demo.ict4apps.com/blogs", "Blogs"},
+                {"CONTACT US", "http://demo.ict4apps.com/contact-us", "Contact Us"}};
+    }
+
+    @Test(dataProvider = "headerMenuLinkRelatedData")
+    public void checkHeaderMenuLinks(String[] data) throws InterruptedException {
+        welcomePage = openWelcomePage();
+        welcomePage.selectItemHeaderMenu(data[0]);
+        Assert.assertEquals(data[1], getCurrentUrl());
+        Assert.assertEquals(data[2], welcomePage.getLastBreadCrumb());
+    }
 
 
 }
